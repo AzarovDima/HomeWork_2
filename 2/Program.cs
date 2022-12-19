50542a1c89b412bce65fd21d237dd9185b007eb7
AzarovDima <ada89885308140@gmail.com>
2022-12-19 23:38:36 +0300

    Решили задачу 2 путем перебора

---
+++ 2/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите любое целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+if (number >= 100 && number < 1000)
+{
+    int a3 = number % 10 / 1;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 1000 && number < 10000)
+{
+    int a3 = number % 100 / 10;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 10000 && number < 100000)
+{
+    int a3 = number % 1000 / 100;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 100000 && number < 1000000)
+{
+    int a3 = number % 10000 / 1000;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 1000000 && number < 10000000)
+{
+    int a3 = number % 100000 / 10000;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 10000000 && number < 100000000)
+{
+    int a3 = number % 1000000 / 100000;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number >= 100000000 && number < 1000000000)
+{
+    int a3 = number % 10000000 / 1000000;
+    Console.WriteLine($"{number} ---> {a3}");
+}
+else if (number < 100)
+{
+    Console.WriteLine($"{number} ---> третьей цифры нет");
+} */
